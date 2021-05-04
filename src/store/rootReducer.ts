e41a70839fbaff03889b02e4a33f8934590ea650
tokai235 <the.summer.of.spiritual@gmail.com>
2021-05-04 20:00:57 +0900

    フォルダ構造を整理

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit'
 
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({
+  // todos: null,
+})
 
 export type RootState = ReturnType<typeof rootReducer>
 
