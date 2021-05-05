d48b30e1b279dfdbf5966b644f8e75cba79d0021
tokai235 <the.summer.of.spiritual@gmail.com>
2021-05-05 15:59:55 +0900

    ファイル名変更

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './screens/App/App.component'
+import App from './screens/App/App'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 import store from './store/rootStore'
